25c9faee2f4f74f9296c3ed88e399d48a20d93e3
ntqvinh <ntqvinh.251103@gmail.com>
2025-07-30 08:59:57 +0700

    Fix Header UI (Desktop)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
   return (
     <div className="flex justify-between items-center p-4 w-full fixed top-0 left-0 bg-white dark:bg-dark z-50 shadow">
       <AlignJustify className="block sm:hidden dark:text-white" onClick={() => setIsShowMenu(true)} />
-      <ul className={`fixed h-dvh sm:relative pl-10 pr-28 py-4 sm:p-0 shadow sm:shadow-none top-0 ${isShowMenu ? 'left-0' : '-left-full'} sm:left-0 bg-white dark:bg-highlight dark:sm:bg-transparent sm:bg-transparent flex-col sm:flex-row flex gap-10 sm:items-center transition-all`}>
+      <ul className={`fixed h-dvh sm:h-auto sm:relative pl-10 pr-28 py-4 sm:p-0 shadow sm:shadow-none top-0 ${isShowMenu ? 'left-0' : '-left-full'} sm:left-0 bg-white dark:bg-highlight dark:sm:bg-transparent sm:bg-transparent flex-col sm:flex-row flex gap-10 sm:items-center transition-all`}>
         {navigations.map((item, index) => (
           <li key={index}>
             <a href={item.value} className="font-medium transition-all hover:text-highlight text-lg dark:text-white" onClick={() => setIsShowMenu(false)}>{item.label}</a>
